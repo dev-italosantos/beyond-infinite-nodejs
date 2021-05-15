81b594bc03b3af3a01ea3a57e54650855b73dec6
dev-santos <italotayllor@gmail.com>
2021-05-15 06:30:18 -0300

    nodejs-infinite-display

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Seja bem-vindo Italo Santos ao App!");
+    res.sendFile(__dirname + "/html/index.html");
 });
 
 app.listen(8090, function(){
