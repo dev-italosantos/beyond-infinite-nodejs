15cf818572ea37dccd89db2653b22e53b260a642
dev-santos <italotayllor@gmail.com>
2021-05-17 21:54:36 -0300

    nodejs-infinite-handlebars

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const Sequelize =  require('sequelize');
+const handlebars = require('express-handlebars')
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/html/index.html");
-});
+
+ 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+const sequelize = new Sequelize('nasa', 'root', 'root', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+app.get('/usuarios', function(req, res){
+    res.render('form')
+})
 
 app.listen(8090, function(){
     console.log(" Servidor inicializado \n URL http://localhost:8090");
