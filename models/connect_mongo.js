80cad6c18e92e98b06c69fb51513366e4d087db7
dev-santos <italotayllor@gmail.com>
2021-06-04 13:18:59 -0300

    nodejs-infinite-modified-models

--- models/connect_mongo.js
+++ models/connect_mongo.js
@@ -9,3 +9,36 @@ mongoose.connect("mongodb://localhost/oibj", {
 }).catch((err) => {
 	console.log("Houve um erro ao se conectar" + err)
 })
+
+
+// Model Usuarios
+//Definindo Model
+const UsuarioSchema = mongoose.Schema({
+	name: {
+		type: String,
+		require: true
+	},
+	sobrenome: {
+		type: String,
+		require: true
+	},
+	email: {
+		type: String,
+		require: true
+	}
+})
+
+//Collection
+mongoose.model('usuarios', UsuarioSchema)
+
+const Test = mongoose.model('usuarios')
+
+new Test({
+	nome: "test",
+	sobrenome: "test",
+	email: "any_@email.com"
+}).save().then(() => {
+	console.log("Usuário criado com sucesso!")
+}).catch((err) => {
+	console.log("Erro#######" + err)
+})
