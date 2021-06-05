0f69e6feadc3b27a392fc7263e326c411ac54e5a
dev-santos <italotayllor@gmail.com>
2021-06-05 18:54:13 -0300

    nodejs-infinite-add-route-group-admin

---
+++ blog_app/routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	res.send("Página principal do paniel ADM")
+})
+
+router.get('/posts', (req, res) => {
+	res.send("Página de posts")
+})
+
+router.get('/categorias', (req, res) => {
+	res.send("P´7 de categorias")
+})
+
+module.exports = router
