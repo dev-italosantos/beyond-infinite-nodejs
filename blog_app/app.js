88ccbd635c2678a4f4846b29d9fce14c25ba6fce
dev-santos <italotayllor@gmail.com>
2021-06-05 18:52:03 -0300

    nodejs-infinite-modified-blog-app

--- blog_app/app.js
+++ blog_app/app.js
@@ -4,7 +4,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 //const mongoose = require('mongoose')
 const app = express()
-
+const admin = require("./routes/admin")
 //Configurações
 	
 	//Body Parser
@@ -14,6 +14,9 @@ const app = express()
 	//Handlebars
 	app.engine('handlebars', handlebars({ extname: 'handlebars', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }))
 	app.set('view engine', 'handlebars')
+
+//Rotas
+app.use('/admin', admin)
 
 //Outros
 const PORT =  8190
